2419161852dc90bac6165977bbb1c7f397fe3b6b
jedi <amajidsinar@gmail.com>
2021-07-16 18:12:30 +0700

    authorized user can withdraw money

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -4,19 +4,23 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0-beta.
 
 
 contract SimpleWallet is Ownable{
-    
     mapping (address => uint) public user;
     
     function addAllowance(address _id, uint _amount) public onlyOwner {
-        user[_id] += _amount;
+        user[_id] = _amount;
     }
     
-    function reduceAllowance(address _id, uint _amount) public onlyOwner {
+    modifier authorizedSubjects(uint _amount){
+        require (isOwner() || user[msg.sender] >= _amount, "YOU ARE NOT ALLOWED");
+        _;
+    }
+    
+    function reduceAllowance(address _id, uint _amount) public authorizedSubjects(_amount) {
         user[_id] -= _amount;
     }
     
     
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+    function withdrawMoney(address payable _to, uint _amount) public authorizedSubjects(_amount) {
         if(!isOwner()) {
             reduceAllowance(msg.sender, _amount);
         }
